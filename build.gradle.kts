b44f3cd16eec72446603f42d4c63698a75432c89
Oscar Westra van Holthe - Kind <oscar@westravanholthe.nl>
2024-09-27 10:47:55 +0200

    Upgrade Gradle & Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.util.*
 plugins {
 	`java-library`
 	id("java")
-	id("org.jetbrains.intellij.platform") version "2.0.1"
+	id("org.jetbrains.intellij.platform") version "2.1.0"
 }
 
 // The first and last supported builds
