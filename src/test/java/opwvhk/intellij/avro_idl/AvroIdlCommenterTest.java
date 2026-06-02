652da842919c29edc25810499e54cd1b85d53d53
Oscar Westra van Holthe - Kind <oscar@westravanholthe.nl>
2026-06-02 15:17:29 +0200

    Refactor comment test to not use internal API

--- src/test/java/opwvhk/intellij/avro_idl/AvroIdlCommenterTest.java
+++ src/test/java/opwvhk/intellij/avro_idl/AvroIdlCommenterTest.java
@@ -1,7 +1,11 @@
 package opwvhk.intellij.avro_idl;
 
-import com.intellij.codeInsight.generation.actions.CommentByBlockCommentAction;
-import com.intellij.codeInsight.generation.actions.CommentByLineCommentAction;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.ex.ActionUtil;
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext;
+import com.intellij.testFramework.TestActionEvent;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
 public class AvroIdlCommenterTest extends BasePlatformTestCase {
@@ -13,41 +17,45 @@ public class AvroIdlCommenterTest extends BasePlatformTestCase {
 
 	public void testLineCommenting() {
 		myFixture.configureByFile("UncommentedLine.avdl");
-		new CommentByLineCommentAction().actionPerformedImpl(getProject(), myFixture.getEditor());
+		performEditorAction("CommentByLineComment");
 		myFixture.checkResultByFile("UncommentedLineCommented.avdl");
 	}
 
 	public void testLineUncommenting() {
 		myFixture.configureByFile("CommentedLine.avdl");
-		new CommentByLineCommentAction().actionPerformedImpl(getProject(), myFixture.getEditor());
+		performEditorAction("CommentByLineComment");
 		myFixture.checkResultByFile("CommentedLineUncommented.avdl");
 	}
 
 	public void testBlockCommenting() {
-
 		myFixture.configureByFile("UncommentedBlock.avdl");
-		new CommentByBlockCommentAction().actionPerformedImpl(getProject(), myFixture.getEditor());
+		performEditorAction("CommentByBlockComment");
 		myFixture.checkResultByFile("UncommentedBlockCommented.avdl");
 	}
 
 	public void testBlockUncommenting() {
-
 		myFixture.configureByFile("CommentedBlock.avdl");
-		new CommentByBlockCommentAction().actionPerformedImpl(getProject(), myFixture.getEditor());
+		performEditorAction("CommentByBlockComment");
 		myFixture.checkResultByFile("CommentedBlockUncommented.avdl");
 	}
 
 	public void testSelectionCommenting() {
-
 		myFixture.configureByFile("UncommentedSelection.avdl");
-		new CommentByBlockCommentAction().actionPerformedImpl(getProject(), myFixture.getEditor());
+		performEditorAction("CommentByBlockComment");
 		myFixture.checkResultByFile("UncommentedSelectionCommented.avdl");
 	}
 
 	public void testSelectionUncommenting() {
-
 		myFixture.configureByFile("CommentedSelection.avdl");
-		new CommentByBlockCommentAction().actionPerformedImpl(getProject(), myFixture.getEditor());
+		performEditorAction("CommentByBlockComment");
 		myFixture.checkResultByFile("CommentedSelectionUncommented.avdl");
+	}
+
+	private void performEditorAction(String action) {
+		var commentByBlockComment = ActionManager.getInstance().getAction(action);
+		AnActionEvent event = TestActionEvent.createTestEvent(SimpleDataContext.builder()
+				.add(CommonDataKeys.PROJECT, myFixture.getEditor().getProject())
+				.add(CommonDataKeys.EDITOR, myFixture.getEditor()).build());
+		ActionUtil.performAction(commentByBlockComment, event);
 	}
 }
